953cc21e61cc321a27f11b7e77b64c2309350523
Waleed-Rasheed <usamapagal75@gmail.com>
2024-03-13 07:31:00 +0500

    work done

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById("img").src="https://github.com/shaikhsufyan/Basic-To-Advance-JavaScript-Project/blob/main/Bulb%20Project/of.png?raw=true"
+function  btnOn() {
+    let img = (document.getElementById("img").src = "https://github.com/shaikhsufyan/Basic-To-Advance-JavaScript-Project/blob/main/Bulb%20Project/on.png?raw=true");
+     }
+  
+  function btnOff() {
+    let imgs = (document.getElementById("img").src = "https://github.com/shaikhsufyan/Basic-To-Advance-JavaScript-Project/blob/main/Bulb%20Project/of.png?raw=true");
+  }
